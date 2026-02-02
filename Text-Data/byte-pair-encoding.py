5d56db111c4b4c905aaf928780fc7848f7ec0d5c
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-02-02 19:44:23 +0100

    Input embedding pipeline(token embeddings + positional embeddings = input embeddings)

--- Text-Data/byte-pair-encoding.py
+++ Text-Data/byte-pair-encoding.py
@@ -42,8 +42,23 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
 
     return dataloader
 
-dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
+# Embedding vectors
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+max_length = 4
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length, 
+    stride=max_length, shuffle=False
+)
 data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
-print("Inputs: \n", inputs)
-print("\nTargets: \n", targets)
+
+token_embeddings = token_embedding_layer(inputs)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
