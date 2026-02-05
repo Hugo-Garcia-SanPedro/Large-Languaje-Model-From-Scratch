94f94219c6b22bcec41377e331a4dfd2a25231d4
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-02-05 18:38:55 +0100

    Text to tokens ids and tokens ids to text

--- Pretraining/generate-text.py
+++ Pretraining/generate-text.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import tiktoken
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257, 
@@ -157,3 +158,36 @@ class GPTModel(nn.Module):
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 model.eval()
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=-1)
+
+    return idx
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding("gpt2")
+
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(start_context, tokenizer),
+    max_new_tokens=10,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
