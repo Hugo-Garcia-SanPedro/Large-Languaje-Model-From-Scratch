c0f6482fdb3329398de65ad0682ebb8bc83e5704
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-02-05 16:59:15 +0100

    Fix a error in the GELU class

--- Implement-GPTmodel/model-architecture.py
+++ Implement-GPTmodel/model-architecture.py
@@ -99,7 +99,7 @@ class GELU(nn.Module):
     def forward(self, x):
         return 0.5 * x * (1 + torch.tanh(
             torch.sqrt(torch.tensor(2.0 / torch.pi)) *
-            (x * 0.044715 * torch.pow(x, 3))
+            (x + 0.044715 * torch.pow(x, 3))
         ))
 
 class FeedForward(nn.Module):
@@ -142,5 +142,27 @@ class ExampleDeepNeuralNetwork(nn.Module):
                 x = x + layer_output
             else:
                 x = layer_output
-        
         return x
+
+layer_sizes = [3, 3, 3, 3, 3, 1]
+sample_input = torch.tensor([[1., 0., -1.]])
+torch.manual_seed(123)
+model_without_shortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=False)
+
+def print_gradients(model, x):
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+print_gradients(model_without_shortcut, sample_input)
+
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=True)
+print_gradients(model_with_shortcut, sample_input)
