b149a74b23fd73163ba34fdea7dc281b2cd34c9c
Hugo-Garcia-SanPedro <garcia.sanpedro.hugo@gmail.com>
2026-02-05 18:08:46 +0100

    The GPT model, generate text

--- Implement-GPTmodel/model-architecture.py
+++ Implement-GPTmodel/model-architecture.py
@@ -287,4 +287,29 @@ total_params_gpt2 = (
 )
 total_size_bytes = total_params * 4
 total_size_mb = total_size_bytes / (1024 * 1024)
-print(f"Total size of the model: {total_size_mb: .2f} MB")
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=-1)
+
+    return idx
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+model.eval()
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
